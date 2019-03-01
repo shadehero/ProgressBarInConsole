30136441d3dbd40566111234f10830381ffbc9ae
Bartek Rybak <gotujcygana@gmail.com>
2019-03-01 09:39:31 -0800

    Add files via upload

---
+++ ProgressBar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ProgressBarInConsole
+{
+	public class ProgressBar
+	{
+		public int X;
+		public int Y;
+		public int Progress; //1-100
+
+		public ProgressBar(int x,int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public void Draw()
+		{
+			Console.SetCursorPosition(X, Y);
+			Console.Write("<[");
+
+			for (int i = 0; i < Progress / 5; i++)
+			{
+				Console.Write('=');
+			}
+
+			Console.SetCursorPosition(22 + X, Y);
+			Console.Write("]> {0}%",Progress);
+		}
+	}
+}
